b73357ca83bf21e033cbb57d4f0d0faf551ec3d7
Francois Martin <franck.martin3d@gmail.com>
2019-09-13 17:16:50 +1000

    started props + state test

--- src/Random.js
+++ src/Random.js
@@ -5,12 +5,37 @@ import TwitterTest from "./components/random/TwitterTest";
 import RandomNavbar from "./components/random/RandomNavbar";
 import Footer from "./components/Footer";
 
+
+// array of objects 
+const data =[
+  {
+  user: "Donal Trump",
+  tweet: "i am an idiot",
+  tweetTotal: 666
+  },
+  {
+    user: "Devil",
+    tweet: " I am the Neigbour of the beast",
+    tweetTotal: 667
+    },
+    {
+      user: "Barrack",
+      tweet: " I was born in honolulu",
+      tweetTotal: 4
+      }
+];
+
+
 class Random extends Component {
   render() {
     return (
       <div className="Container">
         <RandomNavbar />
-        <TwitterTest />
+        <TwitterTest
+          // props
+          title="Tweet  Test Page (prop test)"
+          numberOfClick={0}
+        />
         <Footer />
       </div>
     );
--- src/components/random/TwitterTest.js
+++ src/components/random/TwitterTest.js
@@ -1,59 +1,60 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const tdata= {
-  "created_at": "Wed Oct 10 20:19:24 +0000 2018",
-  "id": 1050118621198921728,
-  "id_str": "1050118621198921728",
-  "text": "To make room for more expression, we will now count all emojis as equal—including those with gender‍‍‍ ‍‍and skin t… https://t.co/MkGjXf9aXm",
-  "user": {},  
-  "entities": {}
- }
+const tdata = {
+  created_at: "Wed Oct 10 20:19:24 +0000 2018",
+  id: 1050118621198921728,
+  id_str: "1050118621198921728",
+  text:
+    "To make room for more expression, we will now count all emojis as equal—including those with gender‍‍‍ ‍‍and skin t… https://t.co/MkGjXf9aXm",
+  user: {},
+  entities: {}
+};
 
-
-
-function displaytweet(){
-  const card = document.getElementById('text');
+function displaytweet() {
+  const card = document.getElementById("text");
 
   const properties = Object.keys(tdata);
-  
-  properties.forEach(function(property){
-      const listItemEl = document.createElement("LI");
-      const textNode = document.createTextNode(property + ": " + tdata[property]);
-      listItemEl.appendChild(textNode);
 
-      card.appendChild(listItemEl);
+  properties.forEach(function(property) {
+    const listItemEl = document.createElement("LI");
+    const textNode = document.createTextNode(property + ": " + tdata[property]);
+    listItemEl.appendChild(textNode);
+
+    card.appendChild(listItemEl);
   });
 }
-function randombody() {
+
+// Rendering function jsx
+const Body = props => {
   return (
-    <div 
-    className="container">
+    <div className="container">
+      <div>
+        <div className="jumbotron jumbotron-fluid text-center border-primary">
+        
+          <h4 className="display-4">{props.title}</h4>
+          <p>Testing how to display tweets</p>
+          {/* second prop test + state test */}
+          <p>Testing button click state: {props.numberOfClick}</p>
+          <button
+            onClick={displaytweet}
+            type="button"
+            className="btn btn-secondary"
+          >
+            Test Button
+          </button>
+        </div>
 
-    
-    <div>
-      <div className="jumbotron jumbotron-fluid text-center border-primary">
-        <h1 className="display-4">Button Test Page</h1>
-        <p>Testing how to display tweets</p>
-        <button
-          onClick={displaytweet}
-          type="button"
-          className="btn btn-secondary"
-        >
-          Test Button
-        </button>
-      </div>
-
-      {/* Card */}
-      <div className="card">
-        <img src="/img/jay.png" className="card-img-top" alt="twiter user" />
-        <div className="card-body">
-          <h5 className="card-title">Twitter Account</h5>
-          <p id="text" className="card-text"></p>
+        {/* Card */}
+        <div className="card">
+          <img src="/img/jay.png" className="card-img-top" alt="twiter user" />
+          <div className="card-body">
+            <h5 className="card-title">Twitter Account</h5>
+            <p id="text" className="card-text"></p>
+          </div>
         </div>
       </div>
     </div>
-    </div>
   );
-}
-export default randombody;
+};
+export default Body;
