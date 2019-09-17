95a92323edc3c1c3e708e80d4d9d34ca312e3cd5
franckmartin3d <franck.martin3d@gmail.com>
2019-09-18 01:17:44 +1000

    Trying to display cards on Onclick

--- src/components/random/TwitterTest.js
+++ src/components/random/TwitterTest.js
@@ -2,37 +2,12 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Card from "./Card";
 
-// // Fake twitter data
-// const tdata = {
-//   created_at: "Wed Oct 10 20:19:24 +0000 2018",
-//   id: 1050118621198921728,
-//   id_str: "1050118621198921728",
-//   text:
-//     "To make room for more expression, we will now count all emojis as equal—including those with gender‍‍‍ ‍‍and skin t… https://t.co/MkGjXf9aXm",
-//   user: {},
-//   entities: {}
-// };
-
-
-
-// // displaying tweet
-// function displaytweet() {
-//   const card = document.getElementById("text");
-
-//   const properties = Object.keys(tdata);
-
-//   properties.forEach(function(property) {
-//     const listItemEl = document.createElement("LI");
-//     const textNode = document.createTextNode(property + ": " + tdata[property]);
-//     listItemEl.appendChild(textNode);
-
-//     card.appendChild(listItemEl);
-//   });
-// }
+let cards = null;
 
 class Body extends React.Component {
   // State
   state = {
+    displayCard: false,
     cards: [
       {
         user: "Donal Trump",
@@ -54,50 +29,86 @@ class Body extends React.Component {
       }
     ]
   };
+ 
 
-  displayTweet = () => {
-    console.log("displaying tweet");
+  // displayTweet = () => {
+  //   console.log("displaying tweet");
+  //   this.setState({
+  //     user: "new user!",
+  //     tweet: "new tweet ",
+  //     date: "new 9/09/99"
+  //   });
+  // };
+
+  // set the display to true or false
+  displayCards = () => {
     this.setState({
-      user: "new user!",
-      tweet: "new tweet ",
-      date: "new 9/09/99"
-    });
-  };
-
-  render() {
-    return (
-      <div className="container">
+        displayCards: !this.state.displayCards
+    })
+  }
+  ifMethod = () =>{
+     if ( this.state.displayCard ) {
+        cards = (
         <div>
-          <div className="jumbotron jumbotron-fluid text-center border-primary">
-            <h4 className="display-4">{this.props.title}</h4>
-            <p>Testing how to display tweets</p>
-            {/* second prop test + state test */}
-            <p> Number of tweets: {this.props.numberOfClick}</p>
-            <button
-              onClick={this.displayTweet}
-              type="button"
-              className="btn btn-secondary"
-            >
-              Display tweet
-            </button>
-          </div>
-
-          {/* CARDS */}
-          <div>
-            {this.state.cards.map(cards => (
+           { this.state.cards.map(cards => (
               <Card
                 user={cards.user}
                 tweet={cards.tweet}
                 date={cards.date}
-                // key={this.props.id.toString()}
-              />
+                ></Card>
             ))}
-          </div>
         </div>
+        )
+      }
+      }
+
+  render() {
+    let cards = null;
+    return (
+
+      <div className="App">
+      <h1>Question Genie</h1>
+      <button className="btn" onClick={this.displayCards}>View     Unanswered Questions</button>
+       
       </div>
-    );
+
+    )}
+
+
+      // <div className="container">
+      //   <div>
+      //     <div className="jumbotron jumbotron-fluid text-center border-primary">
+      //       <h4 className="display-4">{this.props.title}</h4>
+      //       <p>Testing how to display tweets</p>
+      //       {/* second prop test + state test */}
+      //       <p> Number of tweets: {this.props.numberOfClick}</p>
+      //       <button
+      //         onClick={this.displayQuestion}
+      //         type="button"
+      //         className="btn btn-secondary"
+      //       >
+      //         Display tweet
+      //       </button>
+            
+      //     </div>
+      //   </div>
+      // </div>
+
+  //     if ( this.state.displayCard ) {
+  //       cards = (
+  //       <div>
+  //          { this.state.cards.map(cards => (
+  //             <Card
+  //               user={cards.user}
+  //               tweet={cards.tweet}
+  //               date={cards.date}
+  //               ></Card>
+  //           })}
+  //       </div>
+  //       )
+  //     }
+  // }
   }
-}
   
     
 export default Body;
