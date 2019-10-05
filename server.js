1e229f61c020ba3619bb0fbadde532e20a4094f4
Francois Martin <franck.martin3d@gmail.com>
2019-10-05 23:14:31 +1000

    Served my static files

--- server.js
+++ server.js
@@ -3,35 +3,10 @@ const app = express();
 const path = require("path");
 const axios = require("axios");
 
+//  app.use(express.static(path.join(__dirname,'views')))
+app.use(express.static(__dirname + '/client/build/'))
+
 app.get('/api/list', (req,res)=>{
-    //Default test
-    // const list =[
-    //     {id: 1 , firstName: 'John', lastName: 'Doe'},
-    //     {id: 2 , firstName: 'Mila', lastName: 'Lejla'},
-    //     {id: 3 , firstName: 'Nat', lastName: 'Thecat'}
-
-    // ];
-  //  const cards = [
-  //       {
-  //         user: "Donal Trump",
-  //         tweet: "i am an idiot",
-  //         date: "9/12/2019",
-  //         id: 1
-  //       },
-  //       {
-  //         user: "Devil",
-  //         tweet: " I am the Neigbour of the beast",
-  //         date: "6/06/666",
-  //         id: 2
-  //       },
-  //       {
-  //         user: "Barrack",
-  //         tweet: " I was born in honolulu!!",
-  //         date: "1/02/2011",
-  //         id: 3
-  //       }
-  //     ];
-
 
   const twitterCard = [
   {
